1df871d0fd6118c6a480ce800554b9086312dcf2
dama5134 <159628988+dama5134@users.noreply.github.com>
2024-02-16 12:26:19 +0600

    qwerty1

---
+++ android/app/src/main/kotlin/com/example/dmpractice41/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dmpractice41
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Главный экран'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondScreen(),
+              ),
+            );
+            if (result != null) {
+              print('Получены данные с второго экрана: $result');
+            }
+          },
+          child: Text('Перейти к следующему экрану'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Второй экран'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context, 'Данные с второго экрана');
+          },
+          child: Text('Вернуться'),
+        ),
+      ),
+    );
+  }
+}
